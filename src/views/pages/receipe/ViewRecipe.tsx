6a996d8ca3e47823c38fe7e5f5fc9049f263026b
“sanobery” <sanobery@mindfiresolutions.com>
2025-09-23 11:50:43 +0530

    Grid issue solved

--- src/views/pages/receipe/ViewRecipe.tsx
+++ src/views/pages/receipe/ViewRecipe.tsx
@@ -1,27 +1,44 @@
-import React from "react"
-import Grid from "@mui/material/Grid2"
-import { RootState } from "../../../components/redux/Store"
-import { useSelector } from "react-redux"
-import RecipeReviewCard from "./RecipeReviewCard"
+import React from "react";
+import Grid from "@mui/material/Grid"; 
+import { RootState } from "../../../components/redux/Store";
+import { useSelector } from "react-redux";
+import RecipeReviewCard from "./RecipeReviewCard";
 
 const ViewRecipe = React.memo(() => {
-    const recipes = useSelector((state: RootState) => state.recipe.recipes)
-    const searchedRecipes = useSelector((state: RootState) => state.recipe.searchRecipeByIngredient)
-    const displayedRecipes = searchedRecipes.length > 0 ? searchedRecipes : recipes
-    
-    return (
-        <Grid container spacing={3} justifyContent="center">
-            {displayedRecipes.length > 0 ? (
-                displayedRecipes.map((recipe, index) => (
-                    <Grid item key={index} xs={12} sm={6} md={6}>
-                        <RecipeReviewCard {...recipe} />
-                    </Grid>
-                ))
-            ) : (
-                <p>No recipes found.</p>
-            )}
-        </Grid>
-    )
-})
+  const recipes = useSelector((state: RootState) => state.recipe.recipes);
+  const searchedRecipes = useSelector(
+    (state: RootState) => state.recipe.searchRecipeByIngredient
+  );
 
-export default ViewRecipe
+  const displayedRecipes =
+    searchedRecipes.length > 0 ? searchedRecipes : recipes;
+
+  return (
+    <Grid 
+      container 
+      spacing={3} 
+      justifyContent="center"
+      alignItems="stretch"   // ✅ make heights equal
+    >
+      {displayedRecipes.length > 0 ? (
+        displayedRecipes.map((recipe, index) => (
+          <Grid 
+            item 
+            key={index} 
+            xs={12} sm={6} md={4} lg={3} // ✅ better breakpoints
+            display="flex"               // ✅ ensure equal height
+            justifyContent="center"
+          >
+            <RecipeReviewCard {...recipe} />
+          </Grid>
+        ))
+      ) : (
+        <Grid item>
+          <p>No recipes found.</p>
+        </Grid>
+      )}
+    </Grid>
+  );
+});
+
+export default ViewRecipe;
